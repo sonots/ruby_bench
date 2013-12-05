4f5062ebb0eb73bd551fe4b2583962abe4b0f111
Naotoshi Seo <sonots@gmail.com>
2013-12-05 19:10:01 +0900

    raise_vs_if

---
+++ raise_vs_if.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+require 'benchmark'
+n = 1000000
+
+foo = nil
+Benchmark.bm(7, ">rase:", ">if:") do |x|
+  a = x.report("raise") {  n.times{ begin; foo.foo; rescue; end } }
+  b = x.report("if") {  n.times{ if foo.nil?; end } }
+end
+#              user     system      total        real
+#raise     5.210000   0.010000   5.220000 (  5.302661)
+#if        0.120000   0.000000   0.120000 (  0.117799)
