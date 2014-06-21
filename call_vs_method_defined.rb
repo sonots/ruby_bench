9828a712476b2f9f2c59d6039eb69dff2d4393d9
Naotoshi Seo <sonots@gmail.com>
2014-06-22 02:19:54 +0900

    Update call_vs_method_defined.rb

--- call_vs_method_defined.rb
+++ call_vs_method_defined.rb
@@ -7,11 +7,10 @@ Benchmark.ips do |x|
   x.report("call") {  foo }
   x.report("method_defined") {  bar if self.class.method_defined?(:bar) }
 end
+# call wins a bit
 #Calculating -------------------------------------
 #                call     65835 i/100ms
 #      method_defined     66534 i/100ms
 #-------------------------------------------------
 #                call  4514967.9 (±7.7%) i/s -   22449735 in   5.009070s
 #      method_defined  3045663.2 (±6.0%) i/s -   15169752 in   5.000485s
-#
-# call wins
